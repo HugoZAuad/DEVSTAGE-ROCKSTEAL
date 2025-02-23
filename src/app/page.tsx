7697b4efcbbcd4801502bf99c8c282eeeca2f9cf
HugoZAuad <hugozeymer@hotmail.com>
2025-02-23 20:59:36 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { ArrowRight, Copy } from "lucide-react";
+import { Button } from "@/components/Button";
+import { IconButton } from "@/components/IconButton";
+import { Input } from "@/components/Input";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button type="submit">
+        ENVIAR
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <Input type="email" placeholder="Digite o seu e-mail" />
+        <Input type="email" placeholder="Digite o seu e-mail" error />
+      </div>
     </main>
   );
 }
